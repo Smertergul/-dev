4f282147e89594afc1628bb235c25d43a9a4e22d
Smertergul <76622616+Smertergul@users.noreply.github.com>
2021-04-11 16:32:11 +0300

    Add files via upload

---
+++ kayan led.ino
@@ -0,0 +1,90 @@
+int ledPin = 13;
+int ledPin1 = 12;
+int ledPin2 = 11;
+int ledPin3 = 10;
+int ledPin4 = 9;
+int ledPin5 = 8;
+int ledPin6 = 7;
+int ledPin7 = 6;
+/*
+
+*/
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  pinMode(ledPin1, OUTPUT);
+  pinMode(ledPin2, OUTPUT);
+  pinMode(ledPin3, OUTPUT);
+  pinMode(ledPin4, OUTPUT);
+  pinMode(ledPin5, OUTPUT);
+  pinMode(ledPin6, OUTPUT);
+  pinMode(ledPin7, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(ledPin, HIGH);
+  delay(50);
+  digitalWrite(ledPin, LOW);
+  delay(60);
+  digitalWrite(ledPin1, HIGH);
+  delay(50);
+  digitalWrite(ledPin1, LOW);
+  delay(60);
+  digitalWrite(ledPin2, HIGH);
+  delay(50);
+  digitalWrite(ledPin2, LOW);
+  delay(60);
+  digitalWrite(ledPin3, HIGH);
+  delay(50);
+  digitalWrite(ledPin3, LOW);
+  delay(60);
+  digitalWrite(ledPin4, HIGH);
+  delay(50);
+  digitalWrite(ledPin4, LOW);
+  delay(60);
+  digitalWrite(ledPin5, HIGH);
+  delay(50);
+  digitalWrite(ledPin5, LOW);
+  delay(60);
+  digitalWrite(ledPin6, HIGH);
+  delay(50);
+  digitalWrite(ledPin6, LOW);
+  delay(60);
+  digitalWrite(ledPin7, HIGH);
+  delay(50);
+  digitalWrite(ledPin7, LOW);
+  delay(60);
+  digitalWrite(ledPin7, HIGH);
+  delay(50);
+  digitalWrite(ledPin7, LOW);
+  delay(60);
+  digitalWrite(ledPin6, HIGH);
+  delay(50);
+  digitalWrite(ledPin6, LOW);
+  delay(60);
+  digitalWrite(ledPin5, HIGH);
+  delay(50);
+  digitalWrite(ledPin5, LOW);
+  delay(60);
+  digitalWrite(ledPin4, HIGH);
+  delay(50);
+  digitalWrite(ledPin4, LOW);
+  delay(60);
+  digitalWrite(ledPin3, HIGH);
+  delay(50);
+  digitalWrite(ledPin3, LOW);
+  delay(60);
+  digitalWrite(ledPin2, HIGH);
+  delay(50);
+  digitalWrite(ledPin2, LOW);
+  delay(60);
+  digitalWrite(ledPin1, HIGH);
+  delay(50);
+  digitalWrite(ledPin1, LOW);
+  delay(60);
+  digitalWrite(ledPin, HIGH);
+  delay(50);
+  digitalWrite(ledPin, LOW);
+  delay(0);
+  
+}
